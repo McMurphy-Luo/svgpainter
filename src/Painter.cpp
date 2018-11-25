e6a54d33d8d3433e28967ca3011dc01fb79e075e
McMurphy Luo <McMurphy.Luo@gmail.com>
2018-11-25 23:02:43 +0800

    save my work

--- src/Painter.cpp
+++ src/Painter.cpp
@@ -1,20 +1,40 @@
 #include "./Painter.h"
+#include <cassert>
 #include <d2d1.h>
+#include <dxgi.h>
+#include "./MainWindow.h"
 
-namespace // unamed namespace for this file static staff
-{
-
-
-}// end unamed namespace
 
 Painter::Painter(MainWindow* the_window)
   : the_window_(the_window)
+  , dxgi_factory_()
+  , dxgi_adapter_()
+  , dxgi_output_()
+  , swap_chain_()
+  , d3d_device_()
   , render_target_()
 {
+  assert(the_window);
+  HRESULT result = CreateDXGIFactory(__uuidof(IDXGIFactory), reinterpret_cast<void**>(&dxgi_factory_));
+  assert(SUCCEEDED(result));
+  result = dxgi_factory_->EnumAdapters(0, &dxgi_adapter_);
+  assert(SUCCEEDED(result));
+  result = dxgi_adapter_->EnumOutputs(0, &dxgi_output_);
+  DXGI_SWAP_CHAIN_DESC swap_chain_description;
 
+  result = D3D11CreateDeviceAndSwapChain(
+    dxgi_adapter_,
+    D3D_DRIVER_TYPE_HARDWARE, // enforce gpu render
+    NULL,
+    D3D11_CREATE_DEVICE_BGRA_SUPPORT, // for support d2d interoperability 
+    NULL, // default feature level set
+    D3D11_SDK_VERSION,
+
+  );
+  assert(SUCCEEDED(result));
 }
 
-void Painter::Paint(const tinysvg::NodeDelegate<tinysvg::SvgSvg> target)
+void Painter::Paint(const tinysvg::DomString& the_svg_doc_string)
 {
   
 
--- src/Painter.h
+++ src/Painter.h
@@ -4,19 +4,26 @@
 #include "./Config.h"
 #include <atlbase.h>
 #include <d2d1.h>
-#include "./MainWindow.h"
-#include "svg/SvgBase.h"
-#include "svg/SvgSvg.h"
+#include <dxgi.h>
+#include <d3d11.h>
+#include "common/DomString.h"
+
+class MainWindow;
 
 class Painter
 {
 public:
   Painter(MainWindow* the_window);
 
-  void Paint(const tinysvg::NodeDelegate<tinysvg::SvgSvg> target);
+  void Paint(const tinysvg::DomString& the_svg_doc_string);
 
 private:
   MainWindow* the_window_;
+  CComPtr<IDXGIFactory> dxgi_factory_;
+  CComPtr<IDXGIAdapter> dxgi_adapter_;
+  CComPtr<IDXGIOutput> dxgi_output_;
+  CComPtr<IDXGISwapChain> swap_chain_;
+  CComPtr<ID3D11Device> d3d_device_;
   CComPtr<ID2D1RenderTarget> render_target_;
 };
 
